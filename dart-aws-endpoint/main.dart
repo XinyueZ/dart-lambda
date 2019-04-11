374c681fc1015b4019ceb92f115a1d99afdc5093
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-04-12 00:40:04 +0200

    Add dart AWS endpoint sample

---
+++ dart-aws-endpoint/main.dart
@@ -0,0 +1,23 @@
+import 'aws/Auth.dart';
+import 'aws/Service.dart';
+import 'aws/config.dart';
+import 'aws/model/Vehicle.dart';
+
+void main() async {
+  final auth = await Auth(userPoolId, userClientId);
+  try {
+    final session = await auth.login(username, password);
+
+    print("$username =>");
+    print("JWT-Token: ${session.accessToken.jwtToken}");
+
+    Gateway gateway = Gateway(session.accessToken.jwtToken);
+    List<Vehicle> list = await gateway.getVehicles();
+
+    print("List =>");
+    print("${list}");
+  } catch (e) {
+    print("Error:");
+    print(e);
+  }
+}
