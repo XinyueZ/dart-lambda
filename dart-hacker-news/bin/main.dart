dae8461963f8ec8adaa7709e665a5dc94ddae101
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-11-09 03:02:43 +0100

    Add project dart_hacker_news

---
+++ dart-hacker-news/bin/main.dart
@@ -0,0 +1,49 @@
+import 'package:dart_hacker_news/config.dart';
+import 'package:dart_hacker_news/decoder_helper.dart';
+import 'package:dart_hacker_news/domain/hn_item.dart';
+import 'package:dio/dio.dart';
+import 'package:sprintf/sprintf.dart';
+import 'package:dart_hacker_news/domain/extensions.dart';
+
+void main() async {
+  final BaseOptions options = BaseOptions(
+    baseUrl: API_HOST,
+    connectTimeout: 3000,
+    receiveTimeout: 3000,
+    responseType: ResponseType.json,
+  );
+  final Dio dio = Dio(options);
+
+  /**
+   * Read max element (id)
+   */
+  Response response = await dio.get(MAX_ITEM);
+  final String maxItemId = response.toString();
+  print("Max: $maxItemId");
+
+  /**
+   * Read list of elements (id)
+   */
+  final List<HNElement> elements = List();
+  response = await dio.get(TOP_STORIES_ID_LIST);
+  final List<dynamic> feedsMap =
+      DecoderHelper.getJsonDecoder().convert(response.toString());
+  feedsMap.forEach((elementId) {
+    elements.add(HNElement(elementId));
+  });
+  elements.forEach((element) {
+    print("Element id: ${element.toString()}");
+  });
+
+  /**
+   * Build story-list
+   */
+  final List<HNStory> stories = await elements.buildStories(dio);
+  print("========> ${stories.length} stories");
+
+  /**
+   * Build comments for stories
+   */
+  final List<HNComment> comments = await stories.buildComments(dio);
+  print("========> ${comments.length} comments");
+}
